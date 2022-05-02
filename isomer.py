a54f8c2c167dff3c9bb7b6ddc4729977b05e9f6c
Avram Lubkin <aviso@rockhopper.net>
2022-05-02 11:18:37 -0400

    Bump version

--- isomer.py
+++ isomer.py
@@ -25,7 +25,7 @@ import sys
 from tempfile import TemporaryDirectory
 import time
 
-__version__ = '0.1.0'
+__version__ = '0.8.0'
 
 LOGGER = logging.getLogger('isomer')
 LOGGER.addHandler(logging.NullHandler())
